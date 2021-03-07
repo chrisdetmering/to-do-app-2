4590cf160ceca9f33d5bfdff3a68d03025e19620
MyRepo21 <student2020@nym.hush.com>
2021-03-07 13:51:33 -0800

    edits

--- to-do.js
+++ to-do.js
@@ -1,4 +1,4 @@
-  const addButton = document.querySelector("#addButton");
+const addButton = document.querySelector("#addButton");
   const OKButton = document.getElementById("OKButton");
   const cancelButton = document.getElementById("cancelButton");
 
@@ -10,18 +10,27 @@
     
     taskListItem.append(taskInputString);
     taskList.append(taskListItem);
-    taskInputString.value = "";
+    taskInput.value = "";
     })
 
 var taskListItem = document.getElementById("taskList");
 var taskList = document.getElementById("taskList");
+
 taskListItem.addEventListener("click", () => {
+  taskListItem.onauxclick = function(e) {
+    e.preventDefault();
+    taskListItem.style.textDecoration = "line-through";
+  }
+  taskListItem.oncontextmenu = function(e) {
+    e.preventDefault();
+  }
+  /*
 var prompt = window.prompt("1 = Done, 2 = Delete"); 
   if (prompt == 1) {
     taskListItem.style.textDecoration = "line-through";
   } else
   if (prompt == 2) {
     taskListItem.removeChild(taskList); //Uncaught DOMException: Node.removeChild: The node to be removed is not a child of this node
-  }   
+  }   */
 });
   //alert("abc");
