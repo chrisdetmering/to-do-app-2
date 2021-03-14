5d3be702a783ccb43c6648c8e2822d0b61e34213
MyRepo21 <student2020@nym.hush.com>
2021-03-14 08:18:23 -0700

    Edits

--- to-do.js
+++ to-do.js
@@ -1,36 +1,26 @@
 const addButton = document.querySelector("#addButton");
-  const OKButton = document.getElementById("OKButton");
-  const cancelButton = document.getElementById("cancelButton");
 
-    addButton.addEventListener("click", () => {
-    var taskInput = document.getElementById("taskInput");
-    var taskInputString = taskInput.value;
-    var taskList = document.getElementById("taskList");
-    var taskListItem = document.createElement("li");
+addButton.addEventListener("click", () => {
+  var taskInput = document.getElementById("taskInput");
+  let taskInputString = taskInput.value;
+  var taskList = document.getElementById("taskList");
+  let taskListItem = document.createElement("li");
+      
+  taskListItem.append(taskInputString);
+  taskList.append(taskListItem);
+  taskInput.value = "";
+
+  taskListItem.addEventListener("click", (e) => {
+    e.preventDefault();
+    taskListItem.style.textDecoration = "line-through";
+    });
     
-    taskListItem.append(taskInputString);
-    taskList.append(taskListItem);
-    taskInput.value = "";
-    })
-
-var taskListItem = document.getElementById("taskList");
-var taskList = document.getElementById("taskList");
-
-taskListItem.addEventListener("click", () => {
-  taskListItem.onauxclick = function(e) {
+  taskListItem.addEventListener("auxclick", (e) => {
     e.preventDefault();
-    taskListItem.style.textDecoration = "line-through";
-  }
-  taskListItem.oncontextmenu = function(e) {
-    e.preventDefault();
-  }
-  /*
-var prompt = window.prompt("1 = Done, 2 = Delete"); 
-  if (prompt == 1) {
-    taskListItem.style.textDecoration = "line-through";
-  } else
-  if (prompt == 2) {
-    taskListItem.removeChild(taskList); //Uncaught DOMException: Node.removeChild: The node to be removed is not a child of this node
-  }   */
-});
-  //alert("abc");
+    taskList.removeChild(taskListItem);
+    });
+
+  taskListItem.addEventListener("contextmenu", (e) => { 
+    e.preventDefault(); 
+    });
+  });
