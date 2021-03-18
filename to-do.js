02e9da031931c55308b09dc322e871b677e390d9
froggy <chrisdetmering@gmail.com>
2021-03-18 09:54:12 -0700

    Andy Review completed

--- to-do.js
+++ to-do.js
@@ -1,26 +1,26 @@
-const addButton = document.querySelector("#addButton");
 
-addButton.addEventListener("click", () => {
-  var taskInput = document.getElementById("taskInput");
+document.querySelector("#addButton")
+.addEventListener("click", () => {
+  let taskInput = document.getElementById("taskInput");
   let taskInputString = taskInput.value;
-  var taskList = document.getElementById("taskList");
+  let taskList = document.getElementById("taskList");
   let taskListItem = document.createElement("li");
       
   taskListItem.append(taskInputString);
-  taskList.append(taskListItem);
-  taskInput.value = "";
-
-  taskListItem.addEventListener("click", (e) => {
-    e.preventDefault();
-    taskListItem.style.textDecoration = "line-through";
+  
+    taskListItem.addEventListener("click", (e) => {
+      taskListItem.style.textDecoration = "line-through";
     });
     
-  taskListItem.addEventListener("auxclick", (e) => {
-    e.preventDefault();
-    taskList.removeChild(taskListItem);
+    taskListItem.addEventListener("auxclick", (e) => {
+      e.preventDefault();
+      taskList.removeChild(taskListItem);
     });
 
-  taskListItem.addEventListener("contextmenu", (e) => { 
-    e.preventDefault(); 
+    taskListItem.addEventListener("contextmenu", (e) => { 
+      e.preventDefault(); 
     });
+
+    taskList.append(taskListItem);
+    taskInput.value = "";
   });
