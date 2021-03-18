e09222af7904845e967af8b9549aace67d34ea1b
froggy <chrisdetmering@gmail.com>
2021-03-18 12:42:46 -0700

    code review completed

--- to-do.js
+++ to-do.js
@@ -1,26 +1,35 @@
-const addButton = document.querySelector("#addButton");
-
-addButton.addEventListener("click", () => {
-  var taskInput = document.getElementById("taskInput");
-  let taskInputString = taskInput.value;
-  var taskList = document.getElementById("taskList");
-  let taskListItem = document.createElement("li");
+document.querySelector("#addButton")
+.addEventListener("click", () => {
+  const taskInput = document.getElementById("taskInput");
+  const taskInputString = taskInput.value;
+  const taskList = document.getElementById("taskList");
+  const taskListItem = document.createElement("li");
       
   taskListItem.append(taskInputString);
-  taskList.append(taskListItem);
-  taskInput.value = "";
-
+ 
   taskListItem.addEventListener("click", (e) => {
     e.preventDefault();
     taskListItem.style.textDecoration = "line-through";
-    });
+  });
     
   taskListItem.addEventListener("auxclick", (e) => {
     e.preventDefault();
     taskList.removeChild(taskListItem);
-    });
+  });
 
   taskListItem.addEventListener("contextmenu", (e) => { 
     e.preventDefault(); 
-    });
   });
+
+  taskList.append(taskListItem);
+  taskInput.value = "";
+});
+
+//Save todos
+const todos = [{}]; 
+
+//add, delete, toggle
+localStorage.setItem('todos', JSON.stringify(todos));
+
+//get todos
+const todos = JSON.parse(localStorage.getItem("todos"));
